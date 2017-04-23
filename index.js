0054eb86efc0f95759c5b35e4181d7861b7d009f
blond <andrewblond@yandex.com>
2017-04-23 11:48:55 +0300

    feat(preset): add default preset

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 'use strict';
 
-module.exports = {};
+const codePreset = require('./node');
+const stylePreset = require('./style');
+
+module.exports = {
+    parserOptions: { ecmaVersion: 6 },
+    env: { node: true },
+    plugins: ['node', 'promise', 'unicorn'],
+    rules: Object.assign({}, codePreset.rules, stylePreset.rules)
+};
